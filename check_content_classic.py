Fixed weird perspective bug after rotation

check_content_classic.py
@@ -2,26 +2,52 @@ import cv2.cv2 as cv
 import numpy as np
 from get_squares import RESIZED_SQ, AVG_SQUARE
 
+def normalize_image(img):
+    noise = cv.dilate(img, np.ones((7,7),np.uint8))
+    blur = cv.medianBlur(noise, 21)
+    res = 255 - cv.absdiff(img, blur)
+    no_shdw = cv.normalize(res,None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX)
+    return no_shdw
+
 # apply a couple filters to the square, to get a more useful image to work with
 def process_square(square):
     square = cv.cvtColor(square, cv.COLOR_BGR2GRAY)
+    square = normalize_image(square)
     square_m_blur = cv.medianBlur(square, 3)
     square_g_blur = cv.GaussianBlur(square_m_blur, (0, 0), 7)
     square_sharpened = cv.addWeighted(square_m_blur, 1.35, square_g_blur, -0.85, 0)
     _, thresh = cv.threshold(square_sharpened, 5, 255, cv.THRESH_BINARY)
+
+    edges = cv.Canny(cv.bitwise_not(thresh), square.shape[0], square.shape[1])
+
     return thresh
 
 
 
 def check_square(square):
     square = process_square(square)
-    cv.imshow("patch", square)
+    cv.imshow("square", square)
     cv.waitKey(0)
     cv.destroyAllWindows()
     ans = [["" for i in range(9)] for j in range(9)]
     dx, dy = RESIZED_SQ
     stepx, stepy = AVG_SQUARE
     i, j = 0, 0
+    patches_mean_sum = 0
+    for yi in range(0, dy, stepy):
+        if yi + stepy >= dy:  # skip bottom part of image
+            continue
+        j = 0
+        for xi in range(0, dx, stepx):
+            if xi + stepx >= dx:  # last bit of the square is uninteresting
+                continue
+            current_patch = square[yi:yi+stepy, xi:xi+stepx]
+            mean_patch = np.mean(current_patch.squeeze())
+            patches_mean_sum += mean_patch
+
+    mean_patches = patches_mean_sum / 81
+    print(mean_patches)
+
     for yi in range(0, dy, stepy):
         if yi + stepy >= dy: # skip bottom part of image
             continue
@@ -39,15 +65,23 @@ def check_square(square):
                 # print(mean_center_patch)
                 # print("I think it's full")
                 ans[i][j] = "x"
+                # print(mean_center_patch)
+                # cv.imshow("patch", center_patch)
+                # cv.waitKey(0)
+                # cv.destroyAllWindows()
+                # cv.imshow("patch", current_patch)
+                # cv.waitKey(0)
+                # cv.destroyAllWindows()
             else:
                 # print(mean_center_patch)
                 # print("I think it's empty")
                 ans[i][j] = "o"
 
-            if i == 7 and j == 6:
-                cv.imshow("patch", current_patch)
-                cv.waitKey(0)
-                cv.destroyAllWindows()
+            # if i == 7 and j == 6:
+            #     print(mean_center_patch)
+            #     cv.imshow("patch", current_patch)
+            #     cv.waitKey(0)
+            #     cv.destroyAllWindows()
             j += 1
         i += 1
     return ans

get_squares.py
@@ -53,18 +53,31 @@ def crop_squares(imgs, how_many=20, flag=CLASSIC):
             y0 = np.max((np.max(np.array([tly, trry])), 0))
             xn = np.max((np.min(np.array([trx, brx])), 0))
             yn = np.max((np.min(np.array([bly, bry])), 0))
-
-            sudoku_squares += [img_rot[(y0):yn, (x0):xn, :]]
             # print("yamasha")
             print("yamasha")
             print(x0, y0, xn, yn, tlx, tly, blx, bly, trx, trry, brx, bry)
+            old_perspective = cv.transform(old_points, rotation_mat)
+            old_perspective = np.array(old_perspective, np.float32)
+            new_perspective = np.array([[(x0, y0), (xn - 1, y0), (x0, yn - 1), (xn - 1, yn - 1)]], np.float32)
+
+            """
+            A bizzare event unfolds often after rotations. For some reason, rotated images seem to have a weird
+            deviation on their horizontal lines (after rotation), that's usually just small enough to disrupt my
+            predictions, using the center patch idea. Warping the perspective so it should be as expected should
+            hopefully solve this problem.
+            """
+
+            persp_transform = cv.getPerspectiveTransform(old_perspective, new_perspective)
+            img_rot = cv.warpPerspective(img_rot.copy(), persp_transform, (img_rot.shape[0], img_rot.shape[1]))
+            sudoku_squares += [img_rot[y0:yn, x0:xn, :]]
+
             image_copy = img_rot[(y0):yn, (x0):xn, :].copy()
             cv.circle(image_copy, tuple((tlx - x0, tly - y0)), 4, (0, 0, 255), -1)
             cv.circle(image_copy, tuple((trx - x0, trry - y0)), 4, (0, 255, 0), -1)
             cv.circle(image_copy, tuple((blx - x0, bly - y0)), 4, (255, 0, 0), -1)
             cv.circle(image_copy, tuple((brx - x0, bry - y0)), 4, (0, 255, 255), -1)
-
-            # cv.imshow("yamasha", image_copy)
+            #
+            # cv.imshow("yamasrfwrtwrha", image_copy)
             # cv.waitKey(0)
             # cv.destroyAllWindows()
             continue