7cf9a1fd5b48d2c17f292999ad281e05da8705e4
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-11-20 15:17:22 +0200

    small refactor

--- Code/Data_Processing/get_squares.py
+++ Code/Data_Processing/get_squares.py
@@ -21,26 +21,17 @@ def crop_squares(imgs, flag=CLASSIC):
 
         if np.abs(x0 - tlx) >= THRESHOLD_ROTATE or np.abs(x0 - blx) >= THRESHOLD_ROTATE:
             slope = (tly - bly) / (tlx - blx)
-            angle = np.degrees(np.arctan(slope))  # find the angle with the origin
+            angle = np.arctan(slope)  # find the angle with the origin
             if angle > 0:
-                angle = 90 - angle  # everything is inverted, due to (0, 0) being the top, left point, instead of the usual bottom left used in geometry
-                rot_angle = 360 - angle  # rotation seems to be implicitly to the left
+                angle = np.pi/2 - angle  # everything is inverted, due to (0, 0) being the top, left point, instead of the usual bottom left used in geometry
+                rot_angle = 2*np.pi - angle  # rotation seems to be implicitly to the left
             else:
-                angle = np.abs(angle)  # negative angle means that it's been rotated by 360 - abs(val) already
-                angle = 90 - angle  # therefore, we find how much is left to
+                angle = np.pi/2 + angle  # therefore, we find how much is left to
                 rot_angle = angle
+            rot_angle = np.degrees(rot_angle)
             center = ((xn - x0) / 2, (yn - y0) / 2)  # although not the center of the image, it is the center of the square
             rotation_mat = cv.getRotationMatrix2D(center, rot_angle, scale=1.0)
             rot_angle = np.deg2rad(rot_angle)
-            img_rot = cv.warpAffine(src=img[y0:yn, x0:xn, :], M=rotation_mat,
-                                    # dsize=((xn- x0), (yn - y0)))
-                                    dsize=(int((abs(np.sin(rot_angle) * (yn - y0)) +
-                                                abs(np.cos(rot_angle) * (xn - x0)))),
-                                           int(abs(np.sin(rot_angle) * (xn - x0)) +
-                                               abs(np.cos(rot_angle) * (yn - y0)))))
-
-
-            # recalculate where the points are
             if x0 == tlx:
                 old_points = np.array([[(0, tly - y0), (trx - x0, 0), (blx - x0, yn - y0 - 1), (xn - x0 - 1, bry - y0)]])
             else:
@@ -48,6 +39,14 @@ def crop_squares(imgs, flag=CLASSIC):
             new_points = cv.transform(old_points,
                                       rotation_mat).squeeze()
             (tlx, tly), (trx, trry), (blx, bly), (brx, bry) = new_points
+
+
+            img_rot = cv.warpAffine(src=img[y0:yn, x0:xn, :], M=rotation_mat,
+                                    # dsize=((xn- x0), (yn - y0)))
+                                    dsize=(int(np.max([tlx, trx, blx, brx]) - np.min([tlx, trx, blx, brx])),
+                                           int(np.max([tly, trry, bly, bry]) - np.max([tly, trry, bly, bry]))))
+
+            # recalculate where the points are
             x0 = np.max((np.max(np.array([tlx, blx])), 0)) # account for small angles, in which we could have bizzare cases of not exact perfect squares
             y0 = np.max((np.max(np.array([tly, trry])), 0))
             xn = np.max((np.min(np.array([trx, brx])), 0))
@@ -58,10 +57,9 @@ def crop_squares(imgs, flag=CLASSIC):
             new_perspective = np.array([[(x0, y0), (xn - 1, y0), (x0, yn - 1), (xn - 1, yn - 1)]], np.float32)
 
             """
-            A bizzare event unfolds often after rotations. For some reason, rotated images seem to have a weird
-            deviation on their horizontal lines (after rotation), that's usually just small enough to disrupt my
-            predictions, using the center patch idea. Warping the perspective so it should be as expected should
-            hopefully solve this problem.
+            For some reason, rotated images seem to have a weird deviation on their horizontal lines (after rotation),
+            that's usually just small enough to disrupt my predictions, using the center patch idea. Warping the 
+            perspective should hopefully solve this problem.
             """
 
             persp_transform = cv.getPerspectiveTransform(old_perspective, new_perspective)
@@ -74,8 +72,7 @@ def crop_squares(imgs, flag=CLASSIC):
         perspective trick I make sure I get them close enough to being parallel. Due to the approach of checking
         each 55x55 patch, the closeness to actual parallel lines matters a lot for my program.
         """
-        old_perspective = np.array([[(tlx, tly), (trx, trry), (blx, bly), (brx, bry)]])
-        old_perspective = np.array(old_perspective, np.float32)
+        old_perspective = np.array([[(tlx, tly), (trx, trry), (blx, bly), (brx, bry)]], np.float32)
         new_perspective = np.array([[(x0, y0), (xn - 1, y0), (x0, yn - 1), (xn - 1, yn - 1)]], np.float32)
         persp_transform = cv.getPerspectiveTransform(old_perspective, new_perspective)
         img = cv.warpPerspective(img.copy(), persp_transform, (img.shape[0] * 2, img.shape[1] * 2))
